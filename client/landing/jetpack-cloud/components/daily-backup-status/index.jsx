1195943d0cc6e4b6eb60dbaae96a0a9d4b179b95
Rich Collier <rcollier@iris.local>
2020-03-20 17:40:15 -0400

    Jetpack Cloud: Update DailyBackupStatus component styling

--- client/landing/jetpack-cloud/components/daily-backup-status/index.jsx
+++ client/landing/jetpack-cloud/components/daily-backup-status/index.jsx
@@ -36,17 +36,21 @@ class DailyBackupStatus extends Component {
 		const { allowRestore, backupAttempts, moment, translate } = this.props;
 
 		const displayDate = moment( backupAttempts.complete[ 0 ].activityDate ).format(
-			'MMMM Do YYYY, h:mm:ss a'
+			'MMMM Do YYYY, h:mm a'
 		);
 
 		return (
 			<Fragment>
-				<Gridicon icon="cloud-upload" />
+				<Gridicon className="daily-backup-status__status-icon" icon="cloud-upload" />
 				<div className="daily-backup-status__label">
-					{ translate( 'Latest backup completed:' ) }
+					<Fragment>{ translate( 'Latest: ' ) }</Fragment>
+					<Fragment>{ displayDate }</Fragment>
 				</div>
-				<div className="daily-backup-status__date">{ displayDate }</div>
-				<Button className="daily-backup-status__download-button" onClick={ this.triggerDownload }>
+				<Button
+					isPrimary={ false }
+					className="daily-backup-status__download-button"
+					onClick={ this.triggerDownload }
+				>
 					{ translate( 'Download backup' ) }
 				</Button>
 				<Button
@@ -61,20 +65,50 @@ class DailyBackupStatus extends Component {
 	}
 
 	renderFailedBackup() {
-		const { backupAttempts, translate } = this.props;
+		const { backupAttempts, date, moment, translate } = this.props;
 
 		const hasBackupError = backupAttempts.error.length;
 		const errorMessage =
 			hasBackupError && backupAttempts.error[ 0 ].activityDescription[ 0 ].children[ 0 ].text;
 
+		const displayDate = hasBackupError
+			? moment( backupAttempts.error[ 0 ].activityDate ).format( 'MMMM Do, YYYY, h:mm a' )
+			: moment( date ).format( 'MMMM Do, YYYY' );
+
 		return (
 			<Fragment>
 				<Gridicon icon="cross-circle" className="daily-backup-status__gridicon-error-state" />
-
-				<div className="daily-backup-status__date">{ translate( 'Backup error' ) }</div>
-				<div className="daily-backup-status__label">
-					{ hasBackupError ? errorMessage : translate( 'This day has no backups.' ) }
+				<div className="daily-backup-status__warning">
+					{ hasBackupError
+						? translate( 'Backup attempt failed' )
+						: translate( 'Backup not found' ) }
 				</div>
+				<div className="daily-backup-status__label">{ displayDate }</div>
+				<div className="daily-backup-status__message">
+					{ hasBackupError
+						? translate(
+								'A backup for your site was attempted %(dd)s and was not able to be completed.',
+								{ args: { dd: displayDate.replace( ',', ' at' ) } }
+						  )
+						: translate( 'A backup was not attempted for this day.' ) }
+				</div>
+				<div className="daily-backup-status__message">
+					{ translate(
+						'Check out the {{link}}backups help guide{{/link}} or contact our support team to resolve the issue.',
+						{
+							components: {
+								link: <a href="#" />,
+							},
+						}
+					) }
+				</div>
+				<Button
+					className="daily-backup-status__support-button"
+					isPrimary={ false }
+					onClick={ () => {} }
+				>
+					{ translate( 'Contact support' ) }
+				</Button>
 			</Fragment>
 		);
 	}
